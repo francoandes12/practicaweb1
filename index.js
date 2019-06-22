ae049b9fbc19c292a77a39f90dc5a81bce3601ef
franco <francoandes12@gmail.com>
2019-06-22 20:00:27 -0300

    version 1

---
+++ index.js
@@ -0,0 +1,8 @@
+var user = prompt("POR FAVOR INGRESE SU NOMBRE");
+if (user!=""){
+    var saludo=alert("Bienvenido/a "+ user);
+}
+else{
+    var user = prompt("POR FAVOR INGRESE SU NOMBRE");
+    var saludo=alert("Bienvenido/a "+ user);
+}
