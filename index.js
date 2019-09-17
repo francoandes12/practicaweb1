be4186f89d1b64d4c8a6cb9de798186448c4eec3
franco <francoandes12@gmail.com>
2019-09-17 19:45:40 -0300

    version 15.1

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+var ingreso= prompt("Ingrese su nombre");
+var usuario = document.getElementById("micaja");
+usuario.innerHTML="Bienvenido "+ ingreso + " a la mejor pagina de musica electronica!";
+
