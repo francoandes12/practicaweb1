0950991b14e5d535a0c48f9f00766f863f37b1b1
francoandes12 <francoande12@gmail.com>
2019-12-09 22:46:01 -0300

    "version 19.6"

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 /* var ingreso= prompt("Ingrese su nombre");
 var usuario = document.getElementById("micaja");
 usuario.innerHTML="Bienvenido "+ ingreso + " a la mejor pagina de musica electronica!"; */
-
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop(),
+        dh = $(document).height(),
+        wh = $(window).height(),
+        scrollPercent = (scroll / (dh - wh)) * 100;
+    $('#progreso').css('height', scrollPercent + '%');
+})
