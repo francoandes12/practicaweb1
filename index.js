d977729f63b0cdeb3aeae4e15158571c3468b1de
francoandes12 <francoande12@gmail.com>
2020-01-22 14:45:13 -0300

    "version 20.5"

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 /* var ingreso= prompt("Ingrese su nombre");
 var usuario = document.getElementById("micaja");
 usuario.innerHTML="Bienvenido "+ ingreso + " a la mejor pagina de musica electronica!"; */
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop(),
-        dh = $(document).height(),
-        wh = $(window).height(),
-        scrollPercent = (scroll / (dh - wh)) * 100;
-    $('#progreso').css('height', scrollPercent + '%');
-})
+$(document).ready(function(){
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop(),
+            dh = $(document).height(),
+            wh = $(window).height(),
+            scrollPercent = (scroll / (dh - wh)) * 100;
+        $('#progreso').css('height', scrollPercent + '%');
+    });
+    $('#botonmusica').click(function(){
+        $("div#contenedor").toggle();
+    });
+    $('#botonmusica1').click(function(){
+        $("div#contenedor1").toggle();
+    });
+    $('#botonmusica2').click(function(){
+        $("div#contenedor2").toggle();
+    });
+});
